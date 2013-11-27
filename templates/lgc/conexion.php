0e23766d8241a4b0f0a490f1c2051e2ed651a55d
Sebastián <svzosorio@hotmail.com>
2013-11-27 18:18:38 -0500

    Agrego unas cosas de lógica

--- templates/lgc/conexion.php
+++ templates/lgc/conexion.php
@@ -3,7 +3,7 @@ class conexion(){
     public function getConexion(){
          $conexion = mysql_connect('sebastinos.ipagemys ql.com', 'svzosorio', 'Tansoloyo1'); 
         if(!$conexion){
-            die("<script language='javascript'>alert('No se pudo establecer la conexion');</script>");   
+            die("<script language='javascript'>alert('No se pudo establecer la conexion con la base de datos');</script>");   
             return null;
         }
         else{ 
